05f32fd92d7f349c22f9349cf7c20ee8d4ddf181
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-02-18 01:01:03 -0500

    Add project files.

---
+++ MyHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to my world.");
+            Console.ReadLine();
+        }
+    }
+}
